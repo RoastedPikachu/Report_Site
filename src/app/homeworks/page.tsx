13ee54a7ab6842550095bc5ba2404fda2a12d477
Boris Karabut <boriskarabytwork@gmail.com>
2023-09-18 14:56:12 +0300

    Докинул шапку

---
+++ src/app/homeworks/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import TheHeader from "@/widgets/shared/TheHeader";
+
+const Page = () => {
+    return (
+        <>
+            <TheHeader/>
+        </>
+    );
+};
+
+export default Page;
